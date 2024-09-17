3905f20d2f19854bb4e5f48c4a079ae9f16fb451
cjh <1784581164@qq.com>
2024-09-18 01:10:10 +0800

    feat: add traverse

---
+++ src/traverse/index.js
@@ -0,0 +1,43 @@
+import types from "../types";
+
+const { visitorKeys } = types;
+
+export default function traverse(
+  node,
+  visitors,
+  parent,
+  parentPath,
+  key,
+  listKey
+) {
+  const defination = visitorKeys.get(node.type);
+  let visitorFuncs = visitors[node.type] || {};
+
+  if (typeof visitorFuncs === "function") {
+    visitorFuncs = {
+      enter: visitorFuncs,
+    };
+  }
+  const path = new NodePath(node, parent, parentPath, key, listKey);
+  visitorFuncs.enter && visitorFuncs.enter(path);
+
+  if (node.__shouldSkip) {
+    delete node.__shouldSkip;
+    return;
+  }
+
+  if (defination.visitor) {
+    defination.visitor.forEach((key) => {
+      const prop = node[key];
+      if (Array.isArray(prop)) {
+        // 如果该属性是数组
+        prop.forEach((childNode, index) => {
+          traverse(childNode, visitors, node, path, key, index);
+        });
+      } else {
+        traverse(prop, visitors, node, path, key);
+      }
+    });
+  }
+  visitorFuncs.exit && visitorFuncs.exit(path);
+}
